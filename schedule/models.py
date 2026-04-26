c41becc2b67761e8bbc3590fde68cea40e680ec5
Naglis Bumbulis <naglisbumbulis@gmail.com>
2026-04-26 15:57:25 +0100

    Add Meeting model with platform choices

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Meeting(models.Model):
+    PLATFORM_CHOICES = [
+        ('zoom', 'Zoom'),
+        ('teams', 'Microsoft Teams'),
+        ('in_person', 'In Person'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    platform = models.CharField(max_length=50, choices=PLATFORM_CHOICES)
+    message = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
