dee03d6d83d8d8a6b39d44d3e435e17c2874907f
Naglis Bumbulis <naglisbumbulis@gmail.com>
2026-04-27 17:43:48 +0100

    Add weekly and monthly meeting views

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.schedule_home, name='schedule_home'),
     path('create/', views.create_meeting, name='create_meeting'),
+    path('weekly/', views.weekly_view, name='weekly_view'),
+    path('monthly/', views.monthly_view, name='monthly_view'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Meeting
 from .forms import MeetingForm
-from datetime import date
+from datetime import date, timedelta
 
 def schedule_home(request):
     today = date.today()
@@ -20,3 +20,15 @@ def create_meeting(request):
     else:
         form = MeetingForm()
     return render(request, 'schedule/meeting_form.html', {'form': form})
+
+def weekly_view(request):
+    today = date.today()
+    week_end = today + timedelta(days=7)
+    meetings = Meeting.objects.filter(date__gte=today, date__lte=week_end).order_by('date', 'time')
+    return render(request, 'schedule/weekly_view.html', {'meetings': meetings, 'today': today, 'week_end': week_end})
+
+def monthly_view(request):
+    today = date.today()
+    month_end = today + timedelta(days=30)
+    meetings = Meeting.objects.filter(date__gte=today, date__lte=month_end).order_by('date', 'time')
+    return render(request, 'schedule/monthly_view.html', {'meetings': meetings, 'today': today, 'month_end': month_end})
